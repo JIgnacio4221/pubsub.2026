15537edd1325a50fb54f50c732d6688013d281bf
aferre03 <adrian.ferre@alumnos.upm.es>
2026-06-20 18:12:26 +0200

    fase 2

--- broker_node/src/broker/Topic.java
+++ broker_node/src/broker/Topic.java
@@ -1,9 +1,16 @@
 // clase Topic
 package broker;
+
 import pubsub.Event;
 import pubsub.Subscriber;
 
 class Topic {
+    private String name;
+
     public Topic() {
+    }
+
+    public Topic(String name) {
+        this.name = name;
     }
 }
