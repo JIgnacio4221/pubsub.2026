adfaf67ae1bb186ab62219bfe5ec14d1b1ad708a
Jesus Ignacio <jesusignacio.perez@alumnos.upm.es>
2026-06-21 17:09:53 +0200

    Fase7Afaltadeprobar

--- broker_node/src/broker/SubscriberImpl.java
+++ broker_node/src/broker/SubscriberImpl.java
@@ -13,6 +13,11 @@ import pubsub.Event;
 import java.util.List;
 import java.util.ArrayList;
 import java.util.LinkedList;
+import java.nio.file.FileSystems;
+import java.nio.file.PathMatcher;
+import java.nio.file.Paths;
+
+
 class SubscriberImpl extends UnicastRemoteObject implements Subscriber  {
     public static final long serialVersionUID=1234567890L;
     UUID subUUID; // para facilitar depuración
@@ -34,16 +39,32 @@ class SubscriberImpl extends UnicastRemoteObject implements Subscriber  {
 
    
     public int subscribe(String topic, boolean glob) throws RemoteException {
+        int contadorSuscripciones = 0;
         if(glob==false){
             Topic t = ps.getTopic(topic);
             if(t!=null&&!topicsSubscribed.contains(topic)){
                 topicsSubscribed.add(topic);
                 t.addSubscriber(this);
-                return 1;
+                contadorSuscripciones++;
             }
-            return 0;
+        }else{
+            //en lugar de suscribirse a un tema concreto,
+            //  el patron se compara contra todos 
+            // los temas existentes y el suscriptor se suscribe a todos los quee encajan
+            PathMatcher matcher = FileSystems.getDefault().getPathMatcher("glob:" + pattern);
+            
+            for (String nombreTema : ps.topicList()) {
+                if (matcher.matches(Paths.get(nombreTema))) {
+                    Topic t = ps.getTopic(nombreTema);
+                    if (t != null && !topicsSubscribed.contains(nombreTema)) {
+                        topicsSubscribed.add(nombreTema);
+                        t.addSubscriber(this);
+                        contadorSuscripciones++;
+                    }
+                }
+            }
         }
-        return 0;
+        return contadorSuscripciones;
     }
     public Event getEvent() throws RemoteException {
         if (!events.isEmpty()) {
